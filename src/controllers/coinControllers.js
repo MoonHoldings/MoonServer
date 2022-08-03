9b05f2537de7e7ea35e1447457a0bb73bbe20a28
abeer <abeer.technology@gmail.com>
2022-08-04 01:19:52 +0600

    coin search api changes

--- src/controllers/coinControllers.js
+++ src/controllers/coinControllers.js
@@ -3,38 +3,54 @@ const { doc, getDoc } = require("firebase/firestore")
 const { db } = require("../config/firebase")
 const asyncErrorHandler = require("../middlewares/asyncErrorHandler")
 const ErrorHandler = require("../utils/errorHandler")
+const test = require("../test")
 
 exports.getCoins = asyncErrorHandler(async (req, res, next) => {
   const NOMICS_KEY = process.env.NOMICS_KEY
-  let coinsArr, coinsFormed
+  let coinsArr,
+    coinsFormed = []
 
-  const coinRef = await doc(db, "coins", "supported_coins")
-  const coinSnap = await getDoc(coinRef)
+  // const coinRef = await doc(db, "coins", "supported_coins")
+  // const coinSnap = await getDoc(coinRef)
 
-  if (coinSnap.exists()) {
-    const supportedCoins = coinSnap.data().coins
-    const coinIds = supportedCoins.join(",")
+  // if (coinSnap.exists()) {
+  // const supportedCoins = coinSnap.data().coins
+  // const coinIds = supportedCoins.join(",")
 
+  for (let i = 0; i < 3; i++) {
     coinsArr = await axios.get(
-      `https://api.nomics.com/v1/currencies/ticker?key=${NOMICS_KEY}&ids=${coinIds}&interval=1h,1d,7d,30d&per-page=100&page=1&sort=first_priced_at`
+      `https://api.nomics.com/v1/currencies/ticker?key=${NOMICS_KEY}&interval=1d,30d&per-page=100&page=${
+        i + 1
+      }&sort=rank`
     )
-  } else {
-    next(new ErrorHandler("Coins not found", 404))
+
+    coinsArr.data.forEach((coin) => {
+      const coinObj = {
+        id: coin.id,
+        currency: coin.currency,
+        symbol: coin.symbol,
+        name: coin.name,
+        logo_url: coin.logo_url,
+        price: coin.price,
+        circulating_supply: coin.circulating_supply,
+        "1d_change": coin["1d"]["price_change_pct"],
+        "30d_change": coin["30d"]["price_change_pct"],
+      }
+      coinsFormed.push(coinObj)
+    })
   }
 
-  coinsFormed = coinsArr.data.map(coin=>({
-    "id": coin.id,
-    "currency": coin.currency,
-    "symbol": coin.symbol,
-    "name": coin.name,
-    "logo_url": coin.logo_url,
-    "price": coin.price,
-    "circulating_supply": coin.circulating_supply,
-    "1d_change": coin["1d"]["price_change_pct"],
-    "30d_change": coin["30d"]["price_change_pct"],
-  }))
+  // coinsArr = await axios.get(
+  //   `https://api.nomics.com/v1/currencies/ticker?key=${NOMICS_KEY}&ids=${coinIds}&interval=1h,1d,7d,30d&per-page=200&page=1&sort=first_priced_at`
+  // )
+  // } else {
+  // next(new ErrorHandler("Coins not found", 404))
+  // }
+
   res.json({
     success: true,
     coins: coinsFormed,
   })
 })
+
+// https://api.nomics.com/v1/currencies/ticker?key=ee97b194f4a7d1f404c52aceee2a4c6b4464b970&interval=1h,1d,7d,30d&per-page=200&page=1&sort=first_priced_at
